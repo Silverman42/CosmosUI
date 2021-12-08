42d6899d8afcae6c47f1de0c410e43441d800e30
silverman42 <snkeze9@gmail.com>
2021-12-08 04:11:31 +0100

    Change tailwind breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: '320px',
+      md: '576px',
+      lg: '768px',
+      xl: '1024px',
+      '2xl': '1920px',
+    },
     extend: {
       colors: {
         primary: colors.blue,
